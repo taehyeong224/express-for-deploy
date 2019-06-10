e4551e9f2426d50187905f1a5eda72fd32fd2a7e
김태형 <taehyeong224@gmail.com>
2019-06-11 08:14:05 +0900

    test underscore

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const _ = require("underscore");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,8 +9,12 @@ router.get('/', function(req, res, next) {
 
 router.get("/test", (req, res) => {
   console.log("this is test log");
+  const ary1 = [1,2,3]
+  const ary2 = [4,5,6]
+  const ary3 = _.zip(ary1, ary2);
   res.status(200).json({
-    msg: "hihihi"
+    msg: "hihihi",
+    ary3: ary3
   })
 })
 
